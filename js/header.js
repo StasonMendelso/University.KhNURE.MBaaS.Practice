f8e32b4522e389c839cb14e29cd849ef4d98f2cc
Stanislav Hlova <stanislav.hlova@gmail.com>
2024-05-02 20:24:34 +0300

    Add submenu with links for places

--- js/header.js
+++ js/header.js
@@ -1,18 +1,30 @@
 fetchContentAndHandle(
     'views/header.html',
     function (data) {
-            let header = getElementFromBody(data, '.header');
-            if (!header) return;
+        let header = getElementFromBody(data, '.header');
+        if (!header) return;
 
         document.querySelector("header").outerHTML = header.outerHTML;
     }
 );
-function refreshHeader(){
-    if(Backendless.UserService.currentUser){
+
+function refreshHeader() {
+    if (Backendless.UserService.currentUser) {
         //todo check if valid on server
         document.querySelector(".menu__log__item").classList.add("logged");
-    }else{
+    } else {
         document.querySelector(".menu__log__item").classList.remove("logged");
     }
 }
+
+var placesSubmenu = document.querySelector(".places__submenu");
+document.addEventListener("click", event => {
+    if (event.target.getAttribute("data-submenu-name") === "places") {
+        placesSubmenu.classList.add("active");
+        placesSubmenu.classList.remove("disabled");
+    } else if (!event.target.closest("header")) {
+        placesSubmenu.classList.add("disabled");
+        placesSubmenu.classList.remove("active");
+    }
+});
 
