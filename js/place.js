6ca0f6bafc3282905a0c855a077f6d510f8709ee
Stanislav Hlova <stanislav.hlova@gmail.com>
2024-05-06 11:57:50 +0300

    Add google map

--- js/place.js
+++ js/place.js
@@ -1,47 +1,47 @@
-//google map
-(g => {
-    var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document,
-        b = window;
-    b = b[c] || (b[c] = {});
-    var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams,
-        u = () => h || (h = new Promise(async (f, n) => {
-            await (a = m.createElement("script"));
-            e.set("libraries", [...r] + "");
-            for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
-            e.set("callback", c + ".maps." + q);
-            a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
-            d[q] = f;
-            a.onerror = () => h = n(Error(p + " could not load."));
-            a.nonce = m.querySelector("script[nonce]")?.nonce || "";
-            m.head.append(a)
-        }));
-    d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
-})
-({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});
+var urlParams = new URLSearchParams(window.location.search);
+var placeObjectId = urlParams.get('objectId');
+if (placeObjectId) {
+    Backendless.Data.of("Places").findById(placeObjectId)
+        .then(data => {
+            if (!data) {
+                alert("Nothing was found for passed objectId. Check the value of objectId!");
+                return;
+            }
 
-var map;
 
-async function initMap() {
-    // The location of Uluru
-    const position = {lat: -25.344, lng: 131.031};
-    // Request needed libraries.
-    //@ts-ignore
-    const {Map} = await google.maps.importLibrary("maps");
-    const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
+            var map;
 
-    // The map, centered at Uluru
-    map = new Map(document.getElementById("map"), {
-        zoom: 4,
-        center: position,
-        mapId: "DEMO_MAP_ID",
-    });
+            async function initMap() {
+                // The location of Uluru
+                const position = {lat: 49.344, lng: 30.031};
+                const {Map} = await google.maps.importLibrary("maps");
+                const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
 
-    // The marker, positioned at Uluru
-    const marker = new AdvancedMarkerElement({
-        map: map,
-        position: position,
-        title: "Uluru",
-    });
+                map = new Map(document.getElementById("map"), {
+                    zoom: 4,
+                    center: position,
+                    mapId: "DEMO_MAP_ID",
+                });
+
+                const marker = new AdvancedMarkerElement({
+                    map: map,
+                    position: position,
+                    title: "Uluru",
+                });
+            }
+
+            initMap();
+        })
+        .catch(alert);
+
+} else {
+    alert("Pass objectId for getting info about certain place in query param!");
 }
 
-initMap();
+
+
+
+
+
+
+
